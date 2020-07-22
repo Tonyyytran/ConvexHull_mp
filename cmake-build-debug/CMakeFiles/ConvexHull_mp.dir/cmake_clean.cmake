508d9fe914c59f9b331d5437fd7aeb16e557c89d
TonyT <37555343+Tonyyytran@users.noreply.github.com>
2020-07-22 16:07:27 -0700

    upload

---
+++ cmake-build-debug/CMakeFiles/ConvexHull_mp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ConvexHull_mp.dir/main.cpp.o"
+  "ConvexHull_mp.exe"
+  "ConvexHull_mp.pdb"
+  "libConvexHull_mp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ConvexHull_mp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
